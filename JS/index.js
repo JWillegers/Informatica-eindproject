ba7662efe4d96228a1ccdb7677cbff0f3bb154df
MrJnl <148167.jw@gmail.com>
2020-03-16 16:01:43 +0000

    proberen buttons werkend te krjigen

--- JS/index.js
+++ JS/index.js
@@ -1,55 +1,5 @@
-var bubble=true;
-var insertion=false;
-var quick=false;
-var merge=false;
-var play=false;
+var sorting;
 
-
-
-function bubblesortbutton() {
-    if (play==false) {
-        bubble=true;
-        insertion=false;
-        quick=false;
-        merge=false;
-    }
-}
-
-function insertionsortbutton() {
-    if (play==false) {
-        bubble=false;
-        insertion=true;
-        quick=false;
-        merge=false;
-    }
-}
-
-
-function quicksortbutton() {
-    if (play==false) {
-        bubble=false;
-        insertion=false;
-        quick=true;
-        merge=false;
-    }
-}
-
-
-function mergesortbutton() {
-    if (play==false) {
-        bubble=false;
-        insertion=false;
-        quick=false;
-        merge=true;
-    }
-}
-
-
-function playbutton() {
-
-}
-
-if (bubble==true) {
 function setup() {
   canvas = createCanvas(450,450);
   background('silver');
@@ -57,12 +7,24 @@ function setup() {
   //noLoop();
 }
 
-function draw() {
+function draw_bubble() {
   noStroke();
   fill('steelblue');
   ellipse(0,0,800);
   fill('deepskyblue');
-  ellipse(450,450,400); }
+  ellipse(450,450,400); 
+}
+
+function button(choice) {
+ sorting = choice;
+}
+
+function button_play() {
+ alert(sorting); //debuggin button
+}
+
+if (sorting=='bubble') {
+ draw_bubble();
 }
 
 
