5bd0c4cd57d6bf4732eb2bce66c557edf3682042
MrJnl <148167.jw@gmail.com>
2020-03-19 13:21:49 +0000

    setup getallen

--- JS/index.js
+++ JS/index.js
@@ -3,12 +3,16 @@ var play=false;
 
 var Arrbubble=[80,10,90,50,100,30,60,20,40,70];
 var BL=Arrbubble.length;
-var Arrinsertion=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,];
-
+var Arrinsertion=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52];
+var IL=Arrinsertion.length;
+var Arrquick=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+var QL=Arrquick.length;
+var Arrmerge=[1,2,3,4,5,6,7,8,9,10];
+var ML=Arrmerge.length;
 
 function setup() {
   canvas = createCanvas(1900,900);
-  background('silver');
+  background('white');
   canvas.parent('processing');
   //noLoop();
 }
@@ -16,16 +20,20 @@ function setup() {
 function draw() {
     translate(0,900);
     if (sorting=='bubble') {
-
+        clear();
+        fill('red');
+        for (b=1;b<BL;b++) {
+            rect(10+30*b,0,20,-1*Arrbubble[b]);
+        }
     }
     else if (sorting=='insertion') {
-
+        clear();
     }
     else if (sorting=='quick') {
-
+        clear();
     }
     else if (sorting=='merge') {
-
+        clear();
     }
     else {
         text('an error has occured');
@@ -34,6 +42,11 @@ function draw() {
 
 function button(choice) {
  sorting = choice;
+ Arrbubble.sort(function(a, b){return 0.5 - Math.random()});
+ Arrinsertion.sort(function(a, b){return 0.5 - Math.random()});
+ Arrquick.sort(function(a, b){return 0.5 - Math.random()});
+ Arrmerge.sort(function(a, b){return 0.5 - Math.random()});
+ //bron: w3schools -> JS array sort -> sorting an array in random order
 }
 
 function button_play() {
