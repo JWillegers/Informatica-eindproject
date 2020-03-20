69f23b924d484abd2d4e51c3865de3a853b370a8
MrJnl <148167.jw@gmail.com>
2020-03-20 10:30:30 +0000

    code 'veiliger' maken

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,15 @@
 var sorting='bubble';
 var play=false;
+var maxheight=-500;
 
 //https://www.random.org/integer-sets/?sets=1&num=100&min=1&max=100&seqnos=on&commas=on&order=index&format=html&rnd=new
-var Arrbubble=[29, 45, 6, 67, 55, 24, 71, 42, 15, 34, 38, 76, 20, 19, 54, 81, 59, 23, 7, 1, 64, 93, 49, 74, 72, 70, 4, 58, 88, 33, 75, 53, 9, 57, 94, 36, 79, 30, 44, 60, 80, 82, 52, 85, 48, 90, 73, 50, 21, 5, 83, 39, 66, 92, 62, 41, 25, 12, 18, 61, 40, 89, 68, 3, 26, 69, 10, 56, 46, 43, 86, 77, 14, 17, 32, 27, 31, 65, 8, 16, 37, 35, 51, 87, 2, 28, 22, 63, 13, 11, 84, 47, 78, 91];
+var Arrbubble=[29, 45, 6, 67, 55, 24, 71, 42, 15, 34, 38, 76, 20, 19, 54, 81, 59, 23, 7, 1, 64, 49, 74, 72, 70, 4, 58, 88, 33, 75, 53, 9, 57, 36, 79, 30, 44, 60, 80, 82, 52, 85, 48, 90, 73, 50, 21, 5, 83, 39, 66, 62, 41, 25, 12, 18, 61, 40, 89, 68, 3, 26, 69, 10, 56, 46, 43, 86, 77, 14, 17, 32, 27, 31, 65, 8, 16, 37, 35, 51, 87, 2, 28, 22, 63, 13, 11, 84, 47, 78, 91];
 var BL=Arrbubble.length;
 var Arrinsertion=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52];
 var IL=Arrinsertion.length;
 var Arrquick=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
 var QL=Arrquick.length;
-var Arrmerge=[1,2,3,4,5,6,7,8,9,10];
+var Arrmerge=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 var ML=Arrmerge.length;
 
 function setup() {
@@ -23,22 +24,46 @@ function draw() {
     if (sorting=='bubble') {
         clear();
         fill('red');
-        for (b=1;b<BL;b++) {
-            rect(10+20*b,0,20,-5*Arrbubble[b]);
+        for (b=0;b<BL;b++) {
+            rect(40+1830/BL*b,0,1830/BL,maxheight/BL*Arrbubble[b]);
         }
 
         if (play==true) {
             bubblesort();
         }
     }
+
     else if (sorting=='insertion') {
         clear();
+        fill('gray');
+        for (n=0;n<IL;n++) {
+            rect(40+1830/IL*n,0,1830/IL,maxheight/IL*Arrinsertion[n]);
+        }
+
+        if (play==true) {
+            insertionsort();
+        }
     }
+
     else if (sorting=='quick') {
         clear();
+        fill('cyan');
+        for (q=0;q<QL;q++) {
+            rect(40+1830/QL*q,0,1830/QL,maxheight/QL*Arrquick[q]);
+        }
+        if (play==true) {
+            quicksort();
+        }
     }
     else if (sorting=='merge') {
         clear();
+        fill('silver');
+        for (m=0;m<ML;m++) {
+            rect(40+1830/ML*m,0,1830/ML,maxheight/ML*Arrmerge[m]);
+        }
+        if (play==true) {
+            mergesort();
+        }
     }
     else {
         text('an error has occured');
@@ -59,7 +84,7 @@ function button_play() {
     play=true;
     }
 
-function bubblesort() {
+function bubblesort() { //https://www.geeksforgeeks.org/bubble-sort/
     for (i=0; i<BL-1;i++) {
         swapped=false;
 		    for (j=0;j<BL-1;j++) {
@@ -74,9 +99,29 @@ function bubblesort() {
         if (swapped==false) {
             break;
         }
-	}
+    }
+    play=false;
 }
 
+function insertionsort() { //https://www.geeksforgeeks.org/insertion-sort/
+    for(i=0;i<IL;++i) {
+        var key=Arrinsertion[i];
+        var j=i-1;
+        while (j>=0 && Arrinsertion[j]>key) {
+            Arrinsertion[j+1]=Arrinsertion[j];
+            j=j-1;
+        }
+        Arrinsertion[j+1]=key;
+    }
+    play=false;
+}
 
+function quicksort() {
+
+}
+
+function mergesort() { //https://www.geeksforgeeks.org/merge-sort/
+
+}
 
 
